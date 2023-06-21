20123c15dbb3b4802540d8e3fd63830af8a5dcc8
Ayon Sinha <ayon1234kgp@gmail.com>
2023-06-21 15:52:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [joke,Setjoke]=useState(null);
+  let url="https://v2.jokeapi.dev/joke/Any?type=single";
+  let getJokes=()=>{
+      fetch(url).then((data)=>{
+        let js=data.json();
+        // console.log(js);
+        return js;
+      }).then((item)=>{
+        // console.log(item);
+        let val=item.joke;
+        // console.log(val);
+        Setjoke(val);}).catch((err)=>{
+          console.error(err);
+        })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="wrapper">
+            <span>&#128514;</span>
+            <p id="joke">{joke}</p>
+            <button id="btn"onClick={getJokes}>Get Random Joke</button>
+        </div>
   );
 }
 
